c5c18398576c19d867500c81f37274aaf57c0f5a
Elías Hernández <delias16.hernandez@gmail.com>
2026-04-17 07:36:34 +0000

    movie

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,24 @@
-// Write your code here
+class Movie {
+  constructor(title, director, genre, year, rating){
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.year = year,
+    this.rating = rating
+  }
+
+  get overview (){
+    return this.getOverview()
+  }
+
+  getOverview(){
+    return `${this.title}, ${this.genre} 
+      film directed by ${this.director} 
+      was released in ${this.year}. 
+      It received a rating of ${this.rating}.`
+  }
+}
+
+const lordOfRings = new Movie('Lord of the Rings', 'WNS', 'Fantasy', 1990, 5);
+
+console.log(lordOfRings.overview)
