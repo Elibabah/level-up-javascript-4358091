d52af27047521e8b26d19345ccc1fe8f448c28ab
Elías Hernández <delias16.hernandez@gmail.com>
2026-04-18 00:47:17 +0000

    private class and objct

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,27 @@
 // Write your code here
+class User {
+  #username;
+  #password;
+  constructor(username, password, age) {
+    this.#username = username;
+    this.#password = password;
+    this.age = age;
+  }
+}
+
+new User("john_doe", "password123", 30);
+console.log(User.username); // Output: undefined
+console.log(User.password); // Output: undefined
+
+
+
+// const userName = Symbol("username");
+// const password = Symbol("password");
+
+// const user = {
+//   [userName]: 'elias',
+//   [password]: '12345',
+//   age: 33
+// }
+// console.log(user.password);
+// console.log(user[password]);
